2fe8285b8096907d000a17fa71fa2ca76c029df4
Bismark Ade <bismarkade@gmail.com>
2023-01-10 13:44:01 +0100

    script to automate the build process and serve the backend

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,8 @@
-
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
 const planetsRouter = require('./routes/planets/planets.router');
-
 const app = express();
 
 // Cross-Origin-Resource-Sharing: Middleware
@@ -14,7 +13,13 @@ app.use(cors({
 // middleware: It parses incoming requests with JSON payloads
 app.use(express.json());
 
+// serve the frontend with express.static middleware
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
 // Planets Router
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+});
 
 module.exports = app;
