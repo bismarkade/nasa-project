ce8f5fd1ba426afbd46be97b62f15b60ca475198
Bismark Ade <bismarkade@gmail.com>
2023-01-13 22:53:13 +0100

    added the launches endpoint and updated the frontend

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,15 +1,20 @@
 const API_URL = "http://localhost:8000";
 
+// Load planets and return as JSON.
 async function httpGetPlanets() {
-  // Load planets and return as JSON.
  const response = await fetch(`${API_URL}/planets`)
   return await response.json();
 
 }
 
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
   // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
+  const response = await fetch(`${API_URL}/launches`)
+  const fetchedlaunches =  await response.json();
+  return fetchedlaunches.sort((a, b) => {
+    return a.fightNumber - b.flightNumber;
+  });
 }
 
 async function httpSubmitLaunch(launch) {
--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 const app = express();
 
 // Cross-Origin-Resource-Sharing: Middleware
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')))
 
 // Planets Router
 app.use(planetsRouter);
+app.use(launchesRouter);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
