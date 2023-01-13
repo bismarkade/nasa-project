a2cd7494f9c960ff9bc904c46c5cebe3f02606db
Bismark Ade <bismarkade@gmail.com>
2023-01-13 22:02:36 +0100

    reinstalled morgan into the server folder

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,9 @@
+// 
+const launch = {
+    flightNumer: 100,
+    mission: 'Kepler Exploration X',
+    rocket: 'Explorer IS1',
+    launchDate: new Date('December 27, 2030'),
+    destination: 'Kepler-442 b',
+    customer: ['ZTM', 'NASA']
+}
