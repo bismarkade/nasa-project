8874d2d271ea2a2571aff43fc460047c27c29cde
Bismark Ade <bismarkade@gmail.com>
2023-01-14 00:38:40 +0100

    added a check for date property

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -13,7 +13,14 @@ function httpAddNewLaunch(req, res){
             error: 'Missing required launch property',
         });
     }
+    // validating date object
     launch.launchDate = new Date(launch.launchDate);
+    //isNAN --> return true is not a suucessful date
+    if(isNaN(launch.launchDate)){
+        return res.status(400).json({
+            error: 'Invalid launch date',
+        });
+    }
     addNewLaunch(launch);
     
     return res.status(201).json(launch);
